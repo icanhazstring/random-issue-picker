028b7d486da522fd3f0193f2c30678cda6053d3d
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2020-09-29 19:55:12 +0200

    [FEATURE] Add language and label option for #hacktoberfest

--- src/Command/RandomIssueCommand.php
+++ src/Command/RandomIssueCommand.php
@@ -8,6 +8,7 @@ use Icanhazstring\RandomIssuePicker\Model\SearchIssueModel;
 use JMS\Serializer\SerializerBuilder;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -27,6 +28,20 @@ class RandomIssueCommand extends Command
     protected function configure()
     {
         $this->setDescription('Select a random issue and show link and description.');
+
+        $this->addOption(
+            'language',
+            'l',
+            InputOption::VALUE_OPTIONAL,
+            'Which language do you look for e.g. php, js, java, default is php'
+        );
+
+        $this->addOption(
+            'label',
+            '',
+            InputOption::VALUE_OPTIONAL,
+            'Do you want issues for #hacktoberfest?'
+        );
     }
 
     /**
@@ -41,9 +56,12 @@ class RandomIssueCommand extends Command
         $randomPageIndex = random_int(1, 10);
         $randomIssueIndex = random_int(0, 99);
 
+        $language = $input->getOption('language') ?: 'php';
+        $label = $input->getOption('label') ?: 'good first issue';
+
         $res = $this->client->request('GET', 'https://api.github.com/search/issues', [
             'query' => [
-                'q' => 'is:open is:issue language:php label:"good first issue" sort:created-desc',
+                'q' => 'is:open is:issue language:' . $language . ' label:"' . $label . '" sort:created-desc',
                 'per_page' => 100,
                 'page' => $randomPageIndex
             ]
@@ -52,6 +70,11 @@ class RandomIssueCommand extends Command
         $serializer = SerializerBuilder::create()->build();
         /** @var SearchIssueModel $searchIssueModel */
         $searchIssueModel = $serializer->deserialize((string)$res->getBody(), SearchIssueModel::class, 'json');
+
+        if (empty($searchIssueModel->getItems())) {
+            $io->warning("No issue was found with your language: $language and label: $label");
+            return 0;
+        }
 
         $randomIssue = $searchIssueModel->getItems()[$randomIssueIndex];
 
